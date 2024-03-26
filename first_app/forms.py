2e51c28a6e41a94c66bdd3e2aa8c0cb756d461b7
SazzadForazi <sazzad.csebubt@gmail.com>
2024-03-26 10:24:55 +0600

    first commit

---
+++ first_app/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+
+# class contactForm(forms.Form):
+#     name = forms.CharField(label="Enter your Name:")
+    # email = forms.EmailField(label = "User Email")
+    # age = forms.IntegerField()
+    # weight = forms.FloatField()
+    # balance = forms.DecimalField()
+    # age = forms.CharField()
+    # check = forms.BooleanField()
+    # birthday = forms.CharField()
+    # appointment = forms.CharField()
+    # CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    # size = forms.ChoiceField(choices=CHOICES)
+    # MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    # pizza = forms.MultipleChoiceField(choices=MEAL)
+    # file = forms.FileField()
+
+class contactForm(forms.Form):
+    name = forms.CharField(label="Full Name : ", help_text="Total length must be within 70 characters", required=False, error_messages={'required': 'Please enter your name.'},widget = forms.Textarea(attrs = {'id' : 'text_area', 'class' : 'class1 class 2', 'placeholder' : 'Enter your name'},))
+    email = forms.EmailField(label = "User Email")
+    age = forms.CharField(widget=forms.NumberInput)
+    check = forms.BooleanField()
+    birthday = forms.CharField(widget=forms.DateInput(attrs= {'type' : 'date'}))
+    appointment = forms.CharField(widget=forms.DateInput(attrs= {'type' : 'datetime-local'}))
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget = forms.RadioSelect)
+    MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
+
+
+
+
+class PasswordValidationForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        val_pass = self.cleaned_data['password']
+        val_conpass = self.cleaned_data['confirm_password']
+        val_name = self.cleaned_data['name']
+        if val_conpass != val_pass:
+            raise forms.ValidationError("Password doesn't match")
+        if len(val_name) < 15:
+            raise forms.ValidationError("Name must be at least 15 characters")
